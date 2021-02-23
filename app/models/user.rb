73c1543e6c691a486e817781bee96f0124ae91d3
karma0918 <dragonhorsepower@gmail.com>
2021-02-24 00:39:15 +0900

    validatesの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+         with_options presence: true do
+          validates :name
+          validates :password
+          validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+        end
 end
